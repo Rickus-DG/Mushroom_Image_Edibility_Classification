3bab622c88c04031892257ba00eb0d1c3bb5dd3d
zfac037 <zfac037@07b7cfc0-77ee-4e78-894f-051b2ba5a08f>
2021-04-05 19:53:52 +0000

    Sort out the comments and read me to be correct for running of all code

--- final-year-project/machine-learning-project/branches/comparison_of_algorithms/README.txt
+++ final-year-project/machine-learning-project/branches/comparison_of_algorithms/README.txt
@@ -2,5 +2,20 @@ To run any of the notebooks make sure Jupyter and Anaconda are installed
 
 How to install can be found here: https://jupyter.org/install
 
+The results are visible in the main_project_notebook without running the code.
 
-To see all graphs and scoring metrics obtained run the nn-knn.ipynb and decision-tree.ipynb notebooks.
+You will need the following python modules installed if you want to run the code, 
+sklearn
+numpy
+matplotlib
+cv2
+fnmatch
+imutils
+
+All of which install with pip install
+
+To see all image data graphs and scoring metrics obtained run notebooks/main_project_notebook
+
+All testing notebooks are found in the tests folder
+
+All algorithms programmed can be seen in the algorithms folder
--- final-year-project/machine-learning-project/branches/comparison_of_algorithms/scoring/graphing.py
+++ final-year-project/machine-learning-project/branches/comparison_of_algorithms/scoring/graphing.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 class scoreGraph:
     """Used for generating bar graphs for scores"""
 
-    def __init__(self, scores, title=""):
+    def __init__(self, scores, stds, title=""):
         fontSize = 20
         figSize = (10, 5)
         barWidth = 0.2
@@ -13,12 +14,14 @@ class scoreGraph:
         plt.rcParams.update({'font.size': fontSize})
 
         fig, ax = plt.subplots(figsize=figSize)
-        bars = plt.bar(scores.keys(), scores.values(), width=barWidth)
+        bars = plt.bar(scores.keys(), scores.values(), yerr=stds.values(), capsize=10, width=barWidth,
+                       color='lightblue')
 
         for bar in bars:
             height = bar.get_height()
             label_x_pos = bar.get_x() + bar.get_width() / 2
-            ax.text(label_x_pos, height, s=f'{height}', ha='center', va='bottom')
+            ax.text(label_x_pos, 0.9, s=f'{height}', ha='center', va='bottom', color='blue')
+
         plt.yticks(yRange)
         plt.title(title + " Scores On Test-Set")
         plt.show()
